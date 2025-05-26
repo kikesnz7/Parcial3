ecf234b79913e4d94fafc6f3cda36b8bbc285ef3
kikesnz7 <denrique@alu.fpsanantonio.com>
2025-05-26 09:52:51 +0200

    Hemos hecho un cambio en el main, separandio la clase Empleados del main, y haciendo un nuevo metodo para insertar los empleados al array

--- Empleado.java
+++ Empleado.java
@@ -1,7 +1,9 @@
 class Empleado {
-    String nombre;
-    String cargo;
-    double salario;
+    // Atributos de la clase Empleado
+    // nombre, cargo y salario(todos privados)
+    private String nombre;
+    private String cargo;
+    private double salario;
 
     public Empleado(String nombre, String cargo, double salario) {
         this.nombre = nombre;
@@ -17,4 +19,25 @@ class Empleado {
                 ", salario=" + salario +
                 '}';
     }
+    //métodos setter para modificar los atributos
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+    // Métodos getter para acceder a los atributos
+    public String getNombre() {
+        return nombre;
+    }
+    public String getCargo() {
+        return cargo;
+    }
+    public double getSalario() {
+        return salario;
+    }
+    /
 }
